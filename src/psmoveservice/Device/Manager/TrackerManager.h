625f5605a5d728be6f9293a146e2d630dc9439e7
zelmon64 <zelmon64@gmail.com>
2016-06-08 09:23:19 +0100

    Revert "Three PSEyes"
    This reverts commit 23be5a8aea51f28858707ed272346830d0c7b097.

--- src/psmoveservice/Device/Manager/TrackerManager.h
+++ src/psmoveservice/Device/Manager/TrackerManager.h
@@ -57,7 +57,7 @@ public:
 
     bool startup() override;
 
-    static const int k_max_devices = 3;
+    static const int k_max_devices = 2;
     int getMaxDevices() const override
     {
         return TrackerManager::k_max_devices;
