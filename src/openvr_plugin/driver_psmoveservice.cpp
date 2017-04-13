3965c0176f575acc074e371238bd9bd9b61bfa1c
zelmon64 <zelmon64@gmail.com>
2017-04-13 22:35:39 +0100

    PSNavi trigger axis mod

--- src/openvr_plugin/driver_psmoveservice.cpp
+++ src/openvr_plugin/driver_psmoveservice.cpp
@@ -1402,6 +1402,7 @@ CPSMoveControllerLatest::CPSMoveControllerLatest(
 	, m_driverSpaceRotationAtTouchpadPressTime(*k_psm_quaternion_identity)
 	, m_bUseControllerOrientationInHMDAlignment(false)
 	, m_triggerAxisIndex(1)
+	, m_navitriggerAxisIndex(1)
 	, m_thumbstickDeadzone(k_defaultThumbstickDeadZoneRadius)
 	, m_bThumbstickTouchAsPress(true)
 {
@@ -1463,6 +1464,7 @@ CPSMoveControllerLatest::CPSMoveControllerLatest(
 
 			// Trigger mapping
 			m_triggerAxisIndex = LoadInt(pSettings, "psmove", "trigger_axis_index", 1);
+			m_navitriggerAxisIndex = LoadInt(pSettings, "psnavi_button", "trigger_axis_index", m_triggerAxisIndex);
 
 			// Touch pad settings
 			m_bDelayAfterTouchpadPress = 
@@ -2156,7 +2158,9 @@ void CPSMoveControllerLatest::UpdateControllerState()
 				{
 					const PSMPSNavi &naviClientView = m_PSMChildControllerView->ControllerState.PSNaviState;
 
-					NewState.rAxis[m_triggerAxisIndex].x = fmaxf(NewState.rAxis[m_triggerAxisIndex].x, naviClientView.TriggerValue / 255.f);
+					NewState.rAxis[m_navitriggerAxisIndex].x = fmaxf(NewState.rAxis[m_navitriggerAxisIndex].x, naviClientView.TriggerValue / 255.f);
+					if (m_navitriggerAxisIndex != m_triggerAxisIndex)
+						NewState.rAxis[m_navitriggerAxisIndex].y = 0.f;
 				}
 
 				// Trigger SteamVR Events
@@ -2173,6 +2177,20 @@ void CPSMoveControllerLatest::UpdateControllerState()
 					}
 
 					vr::VRServerDriverHost()->TrackedDeviceAxisUpdated(m_unSteamVRTrackedDeviceId, m_triggerAxisIndex, NewState.rAxis[m_triggerAxisIndex]);
+				}
+				if (m_navitriggerAxisIndex != m_triggerAxisIndex && (NewState.rAxis[m_navitriggerAxisIndex].x != m_ControllerState.rAxis[m_navitriggerAxisIndex].x))
+				{
+					if (NewState.rAxis[m_navitriggerAxisIndex].x > 0.1f)
+					{
+						NewState.ulButtonTouched |= vr::ButtonMaskFromId(static_cast<vr::EVRButtonId>(vr::k_EButton_Axis0 + m_navitriggerAxisIndex));
+					}
+
+					if (NewState.rAxis[m_navitriggerAxisIndex].x > 0.8f)
+					{
+						NewState.ulButtonPressed |= vr::ButtonMaskFromId(static_cast<vr::EVRButtonId>(vr::k_EButton_Axis0 + m_navitriggerAxisIndex));
+					}
+
+					vr::VRServerDriverHost()->TrackedDeviceAxisUpdated(m_unSteamVRTrackedDeviceId, m_navitriggerAxisIndex, NewState.rAxis[m_navitriggerAxisIndex]);
 				}
 
 				// Update the battery charge state
